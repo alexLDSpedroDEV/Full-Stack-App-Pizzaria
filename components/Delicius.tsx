e0d440a8783ed42a46978d6f2ffb791bec7fa107
AlexLopesdaSilvaPedro <115034319+alexLDSpedroDEV@users.noreply.github.com>
2023-08-25 14:32:14 -0300

    add cronometro

--- components/Delicius.tsx
+++ components/Delicius.tsx
@@ -6,25 +6,20 @@ import React, { useEffect, useState } from 'react'
 function Delicius() {
 
 
-    const [setTime, time] = useState<any>();
-
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            // chamar as funções diretas
-            
-            let time = new Date();
-            let hora = time.toLocaleTimeString();
-            console.log(hora)
-            
-            setTime(hora)
-        }, 1000);
-        return () => clearInterval(interval);
-      }, []);
-
+    
+    const [time, setTime] = useState<any>();
  
 
+    useEffect(() => {
+         const functionTime = setInterval(() => {
     
+                let time = new Date();
+                let hora = time.toLocaleTimeString();
+
+                setTime(hora)
+                
+            }, 1000);
+    }, []);
 
   return (
     <div className="grid grid-cols-1 p-5 justify-center items-center text-white h-[700px] overflow-hidden bg-[url('.././public/offerBg.png')] bg-local bg-right bg-no-repeat	">
@@ -32,8 +27,8 @@ function Delicius() {
         <div className='w-[70vw] m-auto justify-center items-center'>
             <h2 className='text-[30px] font-extrabold pb-4'>Delicious Burger & French Fry</h2>
             <p className='text-gray-200 text-[13px] pb-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum vitae officia et architecto, ab, praesentium, itaque culpa ratione ad aperiam incidunt dolorum.</p>
-            <p>
-                {time()}
+            <p >
+                {time}
                 </p>
             <button>Order Now</button>
         </div>
