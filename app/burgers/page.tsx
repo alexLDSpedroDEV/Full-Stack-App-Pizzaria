2369b5733a2631a9839d08decda7dba947e27af0
AlexLopesdaSilvaPedro <115034319+alexLDSpedroDEV@users.noreply.github.com>
2023-08-28 12:15:48 -0300

    addd

--- app/burgers/page.tsx
+++ app/burgers/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client"
+import NavBar from '@/components/NavBar'
+import React, { useState } from 'react'
+import FoodItens from '../FoodItens'
+import Image from 'next/image';
 
-function burgers() {
+function page() {
+
+  
+
+  function filtro (data : any) {
+    if (data.tipo == "burgers") {
+      return data;
+    }
+  }
+
+  var pizzas = FoodItens.filter(filtro);
+
+  console.log(pizzas)
+  
+
   return (
-    <div>burgers</div>
+    <div>
+      <NavBar/>
+      <div className=' grid grid-cols-1 w-screen h-[120%] justify-center sm:grid-cols-2 xl:grid-cols-3'>
+        {
+          pizzas.map((item : any,index : any) => (
+            <div key={index}  className='hover:cursor-pointer xl:w-[30vw] sm:w-[45vw] sm:m-3 w-[90vw] m-auto p-5 hover:bg-red-300 '>
+              <h2 className='sm:text-[25px] text-red-700 font-bold text-[30px] p-4'>{item.name}</h2>
+              <Image className='m-auto' src={item.image} alt={item.name} width={280} height={320}/>
+              <div className='mt-9 grid grid-cols-2 justify-between items-center align-middle text-white'> 
+                
+                  <button className='text-center rounded-md p-4 bg-red-600 sm:p-2 sm:text-[12px]'>ADD TO CART</button>
+                  <h2 className='text-end text-red-700 text-[25px] font-bold sm:text-[18px]'>$ {item.value}</h2>  
+                
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
-export default burgers
+export default page
